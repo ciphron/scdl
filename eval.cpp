6e5a16f98c10cf7e54f7aa396535788a55b7b56e
ciphron <ciphron@ciphron.org>
2014-10-06 19:04:31 +0100

    Removed unrelated code from eval.cpp.

--- eval.cpp
+++ eval.cpp
@@ -12,13 +12,8 @@
 
 
 
-struct InputDesc {
-    unsigned int value;
-    unsigned int key_index;
-};
-
-void perform_evaluation(SCDLProgram *circuit, InputDesc *descs,
-                        size_t n_inputs, size_t n_keys);
+void perform_evaluation(SCDLProgram *circuit, uint8_t *inputs,
+                        size_t n_inputs);
 
 
 
@@ -26,9 +21,8 @@ void perform_evaluation(SCDLProgram *circuit, InputDesc *descs,
 
 
 void read_inputs_from_file(char *file_name,
-                           InputDesc *descs,
-                           SCDLProgram *program,
-                           size_t *n_keys)
+                           uint8_t *inputs,
+                           SCDLProgram *program)
 {
 
     size_t n_inputs = program->get_num_variable_inputs();
@@ -38,10 +32,6 @@ void read_inputs_from_file(char *file_name,
 
     std::string line;
     getline(is, line);
-
-    *n_keys = read_numeric<size_t>(line);
-    if (*n_keys > n_inputs)
-        throw "Illegal value for number of keys, must be <= number of inputs";
 
     while (!is.eof()) {
         getline(is, line);
@@ -77,22 +67,15 @@ void read_inputs_from_file(char *file_name,
             if (coeffs.size() != var.len)
                 throw "Length of vector does not match variable length";
             for (int i = 0; i < var.len; i++) {
-                descs[var.input_index + i].value = read_numeric<unsigned int>(coeffs[i]);
+                inputs[var.input_index + i] = read_numeric<unsigned int>(coeffs[i]);
             }
         }
         else {
             if (var.len != 1)
                 throw "Invalid assigment, expected vector";
             unsigned int value = read_numeric<unsigned int>(parts[1]);
-            descs[var.input_index].value = value;
+           inputs[var.input_index] = value;
         }
-
-
-        unsigned int k = read_numeric<unsigned int>(parts[2]);
-        if (k > (*n_keys - 1))
-            throw "Key index out of range";
-        for (int i = 0; i < var.len; i++)
-            descs[var.input_index + i].key_index = k;
     }
 
 
@@ -111,45 +94,34 @@ int main(int argc, char *argv[])
 
         size_t n_inputs = prog->get_num_variable_inputs();
 
-        InputDesc descs[n_inputs];
-        size_t n_keys;
+        uint8_t inputs[n_inputs];
 
-        /* Initialize the values and key indices of unspecied inputs to 0 */
+        /* Initialize the values of inputs to 0 */
         for (int i = 0; i < n_inputs; i++) {
-            descs[i].value = 0;
-            descs[i].key_index = 0;
+            inputs[i] = 0;
         }
 
-        read_inputs_from_file(argv[2], descs, prog, &n_keys);
+        read_inputs_from_file(argv[2], inputs, prog);
 
         
-        perform_evaluation(prog, descs, n_inputs, n_keys);
+        perform_evaluation(prog, inputs, n_inputs);
     }
     catch (const char *e) {
         std::cerr << "ERROR (reading from files) " << e << std::endl;
     }
 }    
 
-void perform_evaluation(SCDLProgram *program, InputDesc *descs,
-                        size_t n_inputs, size_t n_keys)
+void perform_evaluation(SCDLProgram *program, uint8_t *inputs,
+                        size_t n_inputs)
 {
-	srand(time(NULL));
 
+    uint8_t pl_constants[program->get_num_constants()];
+    for (int i = 0; i < program->get_num_constants(); i++) {
+        Constant c = program->get_constant(i);
+        pl_constants[i] = (uint8_t)c.value;
+    }
 
-        /* Plaintext values */
-        uint8_t inputs[n_inputs];
-        for (int i = 0; i < n_inputs; i++)
-            inputs[i] = descs[i].value;
+    uint8_t output = program->run(inputs, pl_constants) % 2;
 
-
-
-        uint8_t pl_constants[program->get_num_constants()];
-        for (int i = 0; i < program->get_num_constants(); i++) {
-            Constant c = program->get_constant(i);
-            pl_constants[i] = (uint8_t)c.value;
-        }
-
-        uint8_t output = program->run(inputs, pl_constants) % 2;
-
-        std::cout <<  "Result is " << (int)output << std::endl;
+    std::cout <<  "Result is " << (int)output << std::endl;
 }
